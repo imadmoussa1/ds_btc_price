503a7a62cf95ca5fcf83bd8fa03d0159b612152a
Imad Moussa <imad.moussa@keeward.com>
2019-02-17 12:30:08 +0200

    Add label to the plot

--- lasso_model.R
+++ lasso_model.R
@@ -22,13 +22,13 @@ lasso_model <- function(x, y) {
   return(model)
 }
 
-predict_model <- function(model, data_to_predict, real_data) {
+predict_model <- function(model, data_to_predict, real_data, label) {
   a  = predict(model, newx = data_to_predict)
-  plot.ts(a)
+  plot.ts(a, sub = label)
   lines(real_data$btc_price,col="green")
   print(mean(a), mean(real_data$btc_price))
 }
-
+set.seed(1)
 training.samples <- createDataPartition(coin_data$btc_price, p = 0.7, list = FALSE)
 train.data  <- coin_data[training.samples, ]
 test.data <- coin_data[-training.samples, ]
@@ -42,17 +42,17 @@ ya <- as.double(coin_data$btc_price)
 test_last_20 <- model.matrix(btc_price ~., coin_data_test)[,-1]
 
 # predict on the 30% of the data
-predict_model(lasso_model(x, y) ,x.test, test.data)
+predict_model(lasso_model(x, y) ,x.test, test.data, 'Validation on 30%, day by day')
 # predict on the last 20 day of the data day by day
-predict_model(lasso_model(x, y), test_last_20, coin_data_test[1])
+predict_model(lasso_model(x, y), test_last_20, coin_data_test[1], 'Test on new data, day by day, trained model on 70%')
 
 # predict on the 30% of the data delay one day
-predict_model(lasso_model(head(x,-1), tail(y,-1)),head(x.test, -1), tail(test.data, -1))
+predict_model(lasso_model(head(x,-1), tail(y,-1)),head(x.test, -1), tail(test.data, -1), 'Validation on 30%, delay one day, trained model on 70%')
 # predict on the last 20 day of the data delay one day
-predict_model(lasso_model(head(x,-1), tail(y,-1)),head(test_last_20, -1), tail(coin_data_test[1], -1))
+predict_model(lasso_model(head(x,-1), tail(y,-1)),head(test_last_20, -1), tail(coin_data_test[1], -1), 'Test on new data, delay one day,  trained model on 70%')
 
 # day by day model on all data trained test on the last 20 day
-predict_model(lasso_model(xa, ya), test_last_20, coin_data_test[1])
+predict_model(lasso_model(xa, ya), test_last_20, coin_data_test[1], 'Test on new data, day by day, trained on all data')
 
 # delay one day model on all of data trained test on the last 20 day
 xa <- head(model.matrix(btc_price~., coin_data)[,-1], -1)
